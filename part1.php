68152387a7b87b23ba821e089fba65107e25f23b
elhorowitz <ericalhorowitz@gmail.com>
2014-03-09 20:15:15 -0400

    small edits

--- part1.php
+++ part1.php
@@ -18,23 +18,25 @@
                 var search = document.getElementById("searches");
                 var results = "";
                 
-                for (i = 0; i < addresses.contacts.length; i = i + 1){
-                    var name = addresses.contacts[i].Name;
-                    var email = addresses.contacts[i].Address;
-                   
-                    if (name.match(search.value) || email.match(search.value)){
-                        results += "<li><a class=\"contact\" href=\"mailto:"+email+"\">"+name+"</a></li>";
-                    } 
+                if (search.value != ""){
+                    for (i = 0; i < addresses.contacts.length; i = i + 1){
+                        var name = addresses.contacts[i].Name;
+                        var email = addresses.contacts[i].Address;
+                       
+                        if (name.toLowerCase().match(search.value.toLowerCase())){
+                            results += "<li><a class=\"contact\" href=\"mailto:"+email+"\">"+name+"</a></li>";
+                        } 
+                    }
+                    
+                    document.getElementById("addresslist").innerHTML = results;
                 }
-                
-                document.getElementById("addresslist").innerHTML = results;
             }
         </script>
     </head>
     <body>
         <h1>Address Book</h1>
         <div id="searchfield">
-            <input id="searches" type="text" name="FirstName" value="Search Addresses" onkeyup="searchIt()">   
+            <input id="searches" type="text" name="FirstName" value="Search Contacts" onkeyup="searchIt()">   
         </div>
         <div id="results">
             <ul id="addresslist">
